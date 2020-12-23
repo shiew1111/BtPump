acad5d29320744aa2f9e4ab5d7ac55f914849e3a
shiew1111 <43846075+shiew1111@users.noreply.github.com>
2020-12-23 17:35:46 +0100

    Initial commit

---
+++ src/web.ts
@@ -0,0 +1,68 @@
+import { WebPlugin } from '@capacitor/core';
+import { BtPumpPluginPlugin } from './definitions';
+
+export class BtPumpPluginWeb extends WebPlugin implements BtPumpPluginPlugin {
+  constructor() {
+    super({
+      name: 'BtPumpPlugin',
+      platforms: ['web'],
+    });
+  }
+
+
+
+  connect(): Promise<{ isConnected: string }> {
+    return new Promise(function (resolve) {
+      resolve({isConnected:"You can't connect with pump from browser!"});
+    })
+  }
+
+
+  pairing(): Promise<{ mac: string }> {
+    return new Promise(function (resolve) {
+      resolve({mac:"You can't pair with pump from browser!"});
+    })
+  }
+
+  GetPumpData(): Promise<{ status: string }> {
+    return new Promise(function (resolve) {
+      resolve({status:"You can't get pump status from browser!"});
+    })
+  }
+
+  disconnect(): Promise<{ isConnected: string }> {
+    return new Promise(function (resolve) {
+      resolve({isConnected:"You can't connect with pump from browser!"});
+    })
+  }
+
+  keyCheck(): Promise<{ key: string }> {
+    return new Promise(function (resolve) {
+      resolve({key:"You can't use pump keys from browser!"});
+    })  }
+
+  keyDown(): Promise<{ key: string }> {
+    return new Promise(function (resolve) {
+      resolve({key:"You can't use pump keys from browser!"});
+    })
+  }
+
+  keyMenu(): Promise<{ key: string }> {
+    return new Promise(function (resolve) {
+      resolve({key:"You can't use pump keys from browser!"});
+    })
+  }
+
+  keyUp(): Promise<{ key: string }> {
+    return new Promise(function (resolve) {
+      resolve({key:"You can't use pump keys from browser!"});
+    })
+  }
+}
+
+const BtPumpPlugin = new BtPumpPluginWeb();
+
+export { BtPumpPlugin };
+
+import { registerWebPlugin } from '@capacitor/core';
+registerWebPlugin(BtPumpPlugin);
